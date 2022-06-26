fad51bbfbf1c2003c1359d385ac96ec5fe0a14f9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-06-26 16:32:05 -0300

    Docs -> Pages -> Scenario 2: Your page paths depend on external data

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "Docs -> Pages -> Static Generation without data"
+git commit -m "Docs -> Pages -> Scenario 2: Your page paths depend on external data"
 git push
 git status
 exit
